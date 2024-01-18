1a62f4559f60b44495faac3a9a51041fb67e90c5
Victor Diez <victordiez1@gmail.com>
2024-01-18 18:05:58 +0100

    Mise en palace su GET all et du GET id

--- back-end/app.js
+++ back-end/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 
-// const stuffRoutes = require('./routes/stuff');
+const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
 
 
@@ -23,9 +23,9 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-// app.use('/api/stuff', stuffRoutes);
+app.use('/api/books', stuffRoutes);
 app.use('/api/auth', userRoutes);
-// app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 
 module.exports = app;
---
+++ back-end/routes/stuff.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+
+const stuffCtrl = require('../controllers/stuff');
+const auth = require('../middleware/auth');
+// const multer = require('../middleware/multer-config');
+
+// router.post('/', auth, multer, stuffCtrl.createBook );
+// router.put('/:id', auth, multer, stuffCtrl.modifyBook );
+// router.delete('/:id', auth, stuffCtrl.deleteBook );
+router.get('/:id', stuffCtrl.getOneBook );
+router.get('/', stuffCtrl.getAllBook );
+
+module.exports = router;
