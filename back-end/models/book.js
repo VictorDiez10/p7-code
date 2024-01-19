7444d550deaa8f6583f7ccc2303f9c5bf3df626f
Victor Diez <victordiez1@gmail.com>
2024-01-19 11:16:28 +0100

    Mise en place de la modification et de la suppression fonctionnel

--- back-end/models/book.js
+++ back-end/models/book.js
@@ -9,7 +9,7 @@ const bookSchema = mongoose.Schema({
     genre: { type: String, required: true},
     ratings : [
         {userId: {type: String, required: true},
-        grade: {type: Number, required: true}}
+        grade: {type: Number, required: true}},
     ],
     averageRating: { type: Number, required: true },
 });
--- back-end/routes/stuff.js
+++ back-end/routes/stuff.js
@@ -4,12 +4,13 @@ const router = express.Router();
 
 const stuffCtrl = require('../controllers/stuff');
 const auth = require('../middleware/auth');
-// const multer = require('../middleware/multer-config');
+const multer = require('../middleware/multer-config');
 
-// router.post('/', auth, multer, stuffCtrl.createBook );
-// router.put('/:id', auth, multer, stuffCtrl.modifyBook );
-// router.delete('/:id', auth, stuffCtrl.deleteBook );
+router.post('/', auth, multer, stuffCtrl.createBook );
+router.put('/:id', auth, multer, stuffCtrl.modifyBook );
+router.delete('/:id', auth, stuffCtrl.deleteBook );
 router.get('/:id', stuffCtrl.getOneBook );
 router.get('/', stuffCtrl.getAllBook );
+// router.get('/bestrating', stuffCtrl.getBestRating );
 
 module.exports = router;
